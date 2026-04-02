40a2d2a1afd6c2aea6782c9165eff67c19b0b237
Qazaidi123 <zaidi.qumar@gmail.com>
2026-04-02 18:52:16 +0530

    Update loadDestinations function to use static data

--- frontend/script.js
+++ frontend/script.js
@@ -1,21 +1,21 @@
 function loadDestinations() {
+    const data = [
+        { name: "Paris", img: "https://source.unsplash.com/300x200/?paris" },
+        { name: "Dubai", img: "https://source.unsplash.com/300x200/?dubai" },
+        { name: "New York", img: "https://source.unsplash.com/300x200/?newyork" },
+        { name: "Goa", img: "https://source.unsplash.com/300x200/?goa" },
+        { name: "Tokyo", img: "https://source.unsplash.com/300x200/?tokyo" }
+    ];
 
-fetch("http://localhost/api.php")
-.then(response => response.json())
-.then(data => {
-
-    let output = "";
+    const container = document.getElementById("destinations");
+    container.innerHTML = "";
 
     data.forEach(place => {
-        output += `
-        <div class="card">
-            <img src="${place.image}">
-            <h3>${place.name}</h3>
-            <p>${place.description}</p>
-        </div>`;
+        container.innerHTML += `
+            <div class="card">
+                <img src="${place.img}" />
+                <h3>${place.name}</h3>
+            </div>
+        `;
     });
-
-    document.getElementById("destinations").innerHTML = output;
-});
 }
-
